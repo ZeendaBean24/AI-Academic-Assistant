7fb8a85466b04b533139910fa7714c57852f0c64
ZeendaBean24 <liuzeen999@gmail.com>
2024-05-06 19:37:22 -0400

    Wrap all code into one app container

---
+++ api/index.js
@@ -0,0 +1,24 @@
+const { app } = require('@azure/functions');
+const { OpenAI } = require("openai");
+
+app.http('getAIResponse', {
+    methods: ['POST'],
+    authLevel: 'anonymous',
+    route: 'ai',
+    handler: async (request, context) => {
+        const openai = new OpenAI({
+            apiKey: process.env.OPENAI_API_KEY
+        });
+
+        const body = await request.json();
+        inputText = body.inputText;
+
+        const completion = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo-0125",
+            messages: [
+              {"role": "system", "content": "You are a assistant helping user analyze their dreams. User will give you the description of the dream, you help me them analyze them. Respond should be in paragraph format, avoid using any markdown symbol"},
+              {"role": "user", "content": `Write an analysis based on the description of my dream: "${inputText}"`}
+            ]
+          });
+    },
+});
